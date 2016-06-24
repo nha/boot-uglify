73cd6d89bb8640ec9f1b9e5c24dc01a2ac324f68
nha <nha@users.noreply.github.com>
2016-06-24 17:02:20 +0200

    add trailing slash to make tests pass on circleCI

--- test/nha/run.clj
+++ test/nha/run.clj
@@ -1,7 +1,8 @@
 (ns nha.run
   (:require  [clojure.test :as t]
              [boot.core :as boot]
-             [adzerk.boot-test :as boot-test])
+             [adzerk.boot-test :as boot-test]
+             [clojure.java.io :as io])
   (:import [java.nio.file Files Path Paths]
            [java.nio.file.attribute BasicFileAttributes]))
 
@@ -11,7 +12,9 @@
 (def js-expected-path "test/resources/samples/js/expected/")
 ;;(def js-output-path "test/resources/samples/js/minified/")
 ;;(def js-output-path (Files/createTempDirectory "boot_uglify" (into-array java.nio.file.attribute.FileAttribute []))) ;; problem: appends timestamp to file names inside this folder
-(def js-output-path (System/getProperty "java.io.tmpdir"))
+
+;; this is inconsistent depending on the platform, so always make sure it ends with a trailing slash
+(def js-output-path (str (.getPath (io/file (System/getProperty "java.io.tmpdir"))) "/"))
 ;; (def js-output-path (.getPath (boot/tmp-dir!)))
 
 (defn setup-tests [test-out-dir]
