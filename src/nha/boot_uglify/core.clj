1e21715c401b74cfef11ceea3b538e1ed9c2f694
Andrea Richiardi <a.richiardi.work@gmail.com>
2017-02-02 11:09:14 -0800

    Add missing require in nha.boot-uglify.core, closes #1

--- src/nha/boot_uglify/core.clj
+++ src/nha/boot_uglify/core.clj
@@ -1,5 +1,7 @@
-(ns nha.boot-uglify.core)
-
+(ns nha.boot-uglify.core
+  (:require [nha.boot-uglify.minify-js]
+            [nha.boot-uglify.gzip]
+            [nha.boot-uglify.brotli]))
 
 (def minify-js nha.boot-uglify.minify-js/minify-js)
 
